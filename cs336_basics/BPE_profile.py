e4988d7792085167353297f0f4c75f8cc5306775
fenzhu <yheverlove27@outlook.com>
2025-07-29 01:00:35 +0800

    BPE profile

---
+++ cs336_basics/BPE_profile.py
@@ -0,0 +1,26 @@
+import base64
+import json
+from BPE import BPE_Train
+
+vocab, merges = BPE_Train(
+    "../data/TinyStoriesV2-GPT4-valid.txt", 10000, ["<|endoftext|>"]
+)
+
+# 序列化字典到第一个文件
+m = 0
+vo = ""
+for k, v in vocab.items():
+    if len(v) > m:
+        m = len(v)
+        vo = str(v)
+print(vo)
+
+
+with open("vocab", "w") as f:
+    json.dump(
+        {str(k): str(v) for k, v in vocab.items()}, f, ensure_ascii=False, indent=2
+    )
+
+# 序列化列表到第二个文件
+with open("merge", "w") as f:
+    json.dump([(str(a), str(b)) for a, b in merges], f, ensure_ascii=False, indent=2)
