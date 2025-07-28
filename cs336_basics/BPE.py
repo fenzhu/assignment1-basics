7065bb02fd0e1ce643fe146c5780bbdd35046d38
fenzhu <jnuyanghao2000@gmail.com>
2025-07-28 14:53:52 +0800

    train bpe pass 2/3

--- cs336_basics/BPE.py
+++ cs336_basics/BPE.py
@@ -14,18 +14,32 @@ def BPE_Example(str):
 
 
 from collections import defaultdict
+import re
+import regex
 
 
-def Merge(indices: dict[tuple[bytes], int], mergeNum=3):
+def Merge(
+    inputIndices: dict[tuple[bytes], int], mergeNum=3
+) -> tuple[dict[tuple[bytes], int], list[tuple[bytes, bytes]]]:
+
+    indices = {}
+    for k, v in inputIndices.items():
+        indices[k] = v
+
+    merges: list[tuple[bytes, bytes]] = []
 
     for _ in range(mergeNum):
+
         count = defaultdict(int)
         for word, freq in indices.items():
             for i, inext in zip(word, word[1:]):
                 count[(i, inext)] += freq
 
+        if len(count) == 0:
+            # 无法再合并了
+            break
+
         pair = max(count, key=lambda k: (count[k], k))
-        print(pair)
 
         newIndices = {}
         for word, freq in indices.items():
@@ -44,7 +58,63 @@ def Merge(indices: dict[tuple[bytes], int], mergeNum=3):
 
             newIndices[tuple(newWord)] = freq
 
+        merges.append(pair)
         indices = newIndices
+    return (indices, merges)
 
 
-BPE_Example(str)
+# BPE_Example(str)
+
+
+def BPE_Train(
+    input_path: str, vocab_size: int, special_tokens: list[str]
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+
+    vocab: dict[int, bytes] = {}
+    merges: list[tuple[bytes, bytes]]
+
+    fileContent = ""
+    with open(input_path, "r", encoding="utf-8") as file:
+        fileContent = file.read()
+    parts = re.split(
+        "|".join([re.escape(special) for special in special_tokens]), fileContent
+    )
+
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
+    # for part in parts:
+
+    allWords = []
+    mergeNum = max(10, vocab_size - 256 - len(special_tokens))
+
+    # for i in range(10):
+    for part in parts:
+        # part = parts[i]
+        # print(part)
+
+        allWords.append(regex.findall(PAT, part.lstrip()))
+
+    wordCount = defaultdict(int)
+    for word in regex.findall(PAT, part.lstrip()):
+        key = tuple([c.encode("utf-8") for c in word])
+        wordCount[key] += 1
+
+    wc, merges = Merge(wordCount, mergeNum)
+
+    for i in range(256):
+        vocab[i] = bytes([i])
+    i = 256
+    for token in special_tokens:
+        vocab[i] = token.encode("utf-8")
+        i += 1
+
+    # print(merges)
+    for a, b in merges:
+        vocab[i] = a + b
+        i += 1
+    # print(vocab)
+
+    return (vocab, merges)
+
+
+# BPE_Train("../data/TinyStoriesV2-GPT4-valid.txt", 300, ["<|endoftext|>"])
--- tests/adapters.py
+++ tests/adapters.py
@@ -10,7 +10,6 @@ import torch
 from torch import Tensor
 
 
-
 def run_linear(
     d_in: int,
     d_out: int,
@@ -453,7 +452,9 @@ def run_cross_entropy(
     raise NotImplementedError
 
 
-def run_gradient_clipping(parameters: Iterable[torch.nn.Parameter], max_l2_norm: float) -> None:
+def run_gradient_clipping(
+    parameters: Iterable[torch.nn.Parameter], max_l2_norm: float
+) -> None:
     """Given a set of parameters, clip their combined gradients to have l2 norm at most max_l2_norm.
 
     Args:
@@ -563,6 +564,9 @@ def get_tokenizer(
     raise NotImplementedError
 
 
+from cs336_basics.BPE import BPE_Train
+
+
 def run_train_bpe(
     input_path: str | os.PathLike,
     vocab_size: int,
@@ -590,4 +594,5 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+
+    return BPE_Train(input_path, vocab_size, special_tokens)
